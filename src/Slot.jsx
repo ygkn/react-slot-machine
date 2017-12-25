003ffa923c1a9c43190940e74a396532c90711a5
Yugo Yagita <2000ygkn0713@gmail.com>
2017-12-26 02:03:45 +0900

    Fix Slot

--- src/Slot.jsx
+++ src/Slot.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import SweetScroll from 'sweet-scroll';
 import PropTypes from 'prop-types';
 
 import { findDOMNode } from 'react-dom';
@@ -7,19 +6,7 @@ import { findDOMNode } from 'react-dom';
 class Slot extends React.Component {
   constructor() {
     super();
-    this.state = { scroller: null };
-  }
-
-  componentDidMount() {
-    this.setState({
-      scroller: new SweetScroll(
-        {
-          duration: this.props.duration,
-          easing: this.props.easing,
-        },
-        this.FrameRef,
-      ),
-    });
+    this.targetRefs = [];
   }
 
   componentDidUpdate(prevProps) {
@@ -27,22 +14,42 @@ class Slot extends React.Component {
 
     this.FrameRef.scrollTop = 0;
 
-    if (this.targetRef) {
-      this.state.scroller.toElement(findDOMNode(this.targetRef));
-    }
+    const target = this.targetRefs[this.props.target];
+
+    if (target == null) return;
+
+    const beforeDistance = findDOMNode(this.targetRefs[this.targetRefs.length - 1]).offsetTop;
+
+    const beforeScroll = this.props.times > 1 ? beforeDistance * (this.props.times - 1) : 0;
+
+    const totalScroll = beforeScroll + target.offsetTop;
+    console.log(target.offsetTop, totalScroll);
+
+    const startTime = Date.now();
+
+    const tick = () => {
+      const elapsed = Date.now() - startTime;
+      console.log(elapsed);
+      if (elapsed > this.props.duration) return;
+
+      this.FrameRef.scrollTop =
+        this.props.easing(elapsed, 0, totalScroll, this.props.duration) % beforeDistance;
+
+      requestAnimationFrame(tick);
+    };
+
+    tick();
   }
 
   render() {
     return (
       <div
         className={this.props.className}
-        style={{ overflow: 'hidden' }}
+        style={{ overflow: 'hidden', position: 'relative' }}
         ref={FrameRef => (this.FrameRef = FrameRef)}
       >
         {this.props.children.map((child, index) =>
-            (index === this.props.target
-              ? React.cloneElement(child, { ref: ref => (this.targetRef = ref) })
-              : child))}
+          React.cloneElement(child, { ref: ref => (this.targetRefs[index] = ref) }))}
       </div>
     );
   }
@@ -50,12 +57,17 @@ class Slot extends React.Component {
 
 Slot.defaultProps = {
   duration: 3000,
-  easing: 'easeOutQuint',
+  easing: function easeOutQuad(elapsed, initialValue, amountOfChange, duration) {
+    return -amountOfChange * (elapsed /= duration) * (elapsed - 2) + initialValue;
+  },
+  times: 1,
 };
 
 Slot.propTypes = {
   duration: PropTypes.number,
   target: PropTypes.number.isRequired,
+  easing: PropTypes.func,
+  times: PropTypes.number,
 };
 
 export default Slot;
